4635e5c6e0f96fd7e925a44c13652568aab52a53
LeiFengDev <LeiFengDev@gmail.com>
2017-06-16 00:43:07 -0400

    provide employee and workgroup hash to check id validation

--- wvchallenges/se-challenge-payroll/solution/app/models/employee.rb
+++ wvchallenges/se-challenge-payroll/solution/app/models/employee.rb
@@ -3,4 +3,11 @@ class Employee < ApplicationRecord
   validates :last_name, presence: true
 
   enum gender: [ :unknown, :male, :female ]
+
+  def self.build_id_list
+    @ids = Array.new
+    Employee.all.map { |e| @ids << e.id }
+    return @ids
+  end
+  
 end
--- wvchallenges/se-challenge-payroll/solution/app/models/workgroup.rb
+++ wvchallenges/se-challenge-payroll/solution/app/models/workgroup.rb
@@ -1,4 +1,14 @@
 class Workgroup < ApplicationRecord
   validates :category, presence: true
   validates :rate, numericality: { greater_than: 0 }
+
+  # class << self; attr_accessor :category_map end
+  # @category_map = build_category_map()
+
+  def self.build_category_map
+    @cate_map = Hash.new
+    Workgroup.all.map { |g| @cate_map[g.category] = g.id }
+    return @cate_map
+  end
+  
 end
