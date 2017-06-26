f6d346fac5d05b63446fc1bbb723fcaa350e32fe
LeiFengDev <LeiFengDev@gmail.com>
2017-06-25 23:51:51 -0400

    add init data for employee and workgroup

--- wvchallenges/se-challenge-payroll/solution/db/seeds.rb
+++ wvchallenges/se-challenge-payroll/solution/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# init Employees
+Employee.create({:first_name => 'Jerry', :last_name => 'Land', :gender => 1, :description => 'test employee 1'})
+Employee.create({:first_name => 'Larry', :last_name => 'Ocean', :gender => 1, :description => 'test employee 2'})
+Employee.create({:first_name => 'Elissabath', :last_name => 'Norland', :gender => 2, :description => 'test employee 3'})
+Employee.create({:first_name => 'John', :last_name => 'Parker', :gender => 1, :description => 'test employee 4'})
+Employee.create({:first_name => 'Mith', :last_name => 'Konan', :gender => 0, :description => 'test employee 5'})
+
+# init Workgroups
+Workgroup.create({:category => 'A', :name => 'Junior', :rate => 20.0})
+Workgroup.create({:category => 'B', :name => 'Senior', :rate => 30.0})
