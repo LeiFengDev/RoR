327949b5a2a893dc5711ce017ce8b9d7e88aaa67
LeiFengDev <LeiFengDev@gmail.com>
2017-06-28 15:52:12 -0400

    update seed to init table more flexible

--- wvchallenges/se-challenge-payroll/solution/db/seeds.rb
+++ wvchallenges/se-challenge-payroll/solution/db/seeds.rb
@@ -7,12 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # init Employees
-Employee.create({:first_name => 'Jerry', :last_name => 'Land', :gender => 1, :description => 'test employee 1'})
-Employee.create({:first_name => 'Larry', :last_name => 'Ocean', :gender => 1, :description => 'test employee 2'})
-Employee.create({:first_name => 'Elissabath', :last_name => 'Norland', :gender => 2, :description => 'test employee 3'})
-Employee.create({:first_name => 'John', :last_name => 'Parker', :gender => 1, :description => 'test employee 4'})
-Employee.create({:first_name => 'Mith', :last_name => 'Konan', :gender => 0, :description => 'test employee 5'})
+(1..10).each do |i|
+  Employee.create({:first_name => "first_#{i}", :last_name => "last_#{i}", :gender => rand(0..2), :description => "test employee #{i}"})
+end
 
 # init Workgroups
-Workgroup.create({:category => 'A', :name => 'Junior', :rate => 20.0})
-Workgroup.create({:category => 'B', :name => 'Senior', :rate => 30.0})
+('A'..'E').each_with_index do |s, i| 
+  Workgroup.create({:category => s, :name => "group #{s}", :rate => (20.0 + 10.0 * i)})
+end
